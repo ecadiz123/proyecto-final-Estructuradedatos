5177500c3531294239b46356d6b529e0e25c953f
esteban <ecadiz@ing.ucsc.cl>
2024-06-25 11:23:26 -0400

    comienzo de la main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
-#include "HashTable/HashTable.hpp"
-#include "HashTable/HashEncadenado.hpp"
-#include "queue/QueueADT.hpp"
+#include "HashTable/HashEncadenado.hpp" 
 #include "queue/ListQueue.hpp"
+#include <iostream>
+
+
+int main(){
+
+HashEncadenado fichas;//tabla donde se van a guardar las fichas
+ListQueue normal, urgencia;//fila de urgencia y la fila normal
+
+
+
+
+
+
+}
