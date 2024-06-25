ca6cc48d56f45952327fd6a50914ad91a3466372
esteban <ecadiz@ing.ucsc.cl>
2024-06-25 13:08:05 -0400

    parte de avance en ficha main

--- HashTable/HashTable.hpp
+++ HashTable/HashTable.hpp
@@ -1,27 +1,31 @@
-#ifndef HASH_TABLE_T 
+#ifndef HASH_TABLE_T
 #define HASH_TABLE_T
-
+#include <string>
 typedef int value_t;
-typedef int key_t;//en este caso int portque es el rut sin ultimo digito
+typedef int key_t; // en este caso int portque es el rut sin ultimo digito
 
-struct element_t//Datos de la ficha
+struct element_t // Datos de la ficha
 {
-    key_t key;//Clave de la ficha.
-    //aqui definir que va a necesitar ficha
-    
-    value_t value;
 
+    int rut;
+    char digitoverificador;
+    key_t key; // Clave de la ficha.
+    // aqui definir que va a necesitar ficha
+
+    std::string motivodeconsulta;
+    std::string antecedentesdentales;
+    int piezasatratar;//las piezas dentales tienen numero
     element_t *next;
-    
 };
 
-class HashTable{
-    public:
-        virtual bool empty() = 0;
-        virtual int size() = 0;
-        virtual element_t find(key_t) = 0;
-        virtual void insert(element_t) = 0;
-        virtual void remove(key_t) = 0;
+class HashTable
+{
+public:
+    virtual bool empty() = 0;
+    virtual int size() = 0;
+    virtual element_t find(key_t) = 0;
+    virtual void insert(element_t) = 0;
+    virtual void remove(key_t) = 0;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@
 
 int main()
 {
-
-    HashEncadenado fichas;      // tabla donde se van a guardar las fichas
-    ListQueue normal, urgencia; // fila de urgencia y la fila normal
+    HashEncadenado guardadodeficha; // tabla donde se van a guardar las fichas
+    ListQueue normal, urgencia;     // fila de urgencia y la fila normal
     int ingreso;
 
     // variables fila
@@ -14,89 +13,114 @@ int main()
     int ingresofila, turno;
 
     // variables fichas
+    element_t fichaingreso;
+    int ingresoficha;
+    int rut;
 
     std::cout << "Bienvenido" << "\n";
-do
-{
-    /* code */
-
-
-    std::cout << "Ingrese que desea hacer" << "\n";
-    std::cout << "1.Organizar clientes en espera" << "\n";
-    std::cout << "2.Trabajar con fichas" << "\n";
-    std::cout << "3. Salir" << "\n";
-    std::cout << "(Ingrese solo numeros)" << "\n";
-
-    std::cin >> ingreso;
-    switch (ingreso)
+    do // se repite hasta que salga
     {
-    case 1:
-
         std::cout << "Ingrese que desea hacer" << "\n";
-        std::cout << "1. Ver quien debe pasar" << "\n";
-        std::cout << "2. Ingresar pacientes" << "\n";
+        std::cout << "1.Organizar clientes en espera" << "\n";
+        std::cout << "2.Trabajar con fichas" << "\n";
+        std::cout << "3. Salir" << "\n";
+        std::cout << "(Ingrese solo numeros)" << "\n";
 
-        std::cin >> turno;
-
+        std::cin >> ingreso;
+        switch (ingreso)
         {
-            switch (turno)
+        case 1:
+
+            std::cout << "Ingrese que desea hacer" << "\n";
+            std::cout << "1. Ver quien debe pasar" << "\n";
+            std::cout << "2. Ingresar pacientes" << "\n";
+
+            std::cin >> turno;
+
             {
-            case 2: // Ingreso de pacientes
-
-                std::cout << "Ingrese nombre y apellido junto de paciente" << "\n";
-
-                std::cin >> nombre;
-
-                std::cout << "Ingrese a que fila lo desea enviar" << "\n";
-
-                std::cout << "1. Atencion Normal" << "\n";
-                std::cout << "2. Atencion de Urgencia" << "\n";
-
-                std::cin >> ingresofila;
-                if (ingresofila == 1)
+                switch (turno)
                 {
-                    normal.enqueue(nombre);
+                case 2: // Ingreso de pacientes
+
+                    std::cout << "Ingrese nombre y apellido junto de paciente" << "\n";
+
+                    std::cin >> nombre;
+
+                    std::cout << "Ingrese a que fila lo desea enviar" << "\n";
+
+                    std::cout << "1. Atencion Normal" << "\n";
+                    std::cout << "2. Atencion de Urgencia" << "\n";
+
+                    std::cin >> ingresofila;
+                    if (ingresofila == 1)
+                    {
+                        normal.enqueue(nombre);
+                    }
+                    else
+                        urgencia.enqueue(nombre);
+
+                    break;
+                case 1: // quien debe pasar
+
+                    if (urgencia.empty() == true)
+                    {
+                        std::cout << normal.front() << "\n";
+
+                        std::cout << "1" << "\n";
+                        normal.dequeue();
+
+                        std::cout << "2" << "\n";
+                    }
+                    else
+                    {
+
+                        std::cout << urgencia.front() << "\n";
+                        urgencia.dequeue();
+                    }
+
+                    break;
+                default:
+                    std::cout << "Error no siguio instrucciones" << "\n";
+                    break;
                 }
-                else
-                    urgencia.enqueue(nombre);
+                break;
+            case 2:
+
+                std::cout << "Ingrese que desea hacer" << "\n";
+
+                std::cout << "1. Consultar ficha" << "\n";
+                std::cout << "2. Añadir/modificar/eliminar ficha" << "\n";
+                std::cin >> ingresoficha;
+                {
+
+                    switch (ingresoficha)
+                    {
+                    case 1: // consulta ficha
+
+                        std::cout << "Ingrese rut sin digito " << "\n";
+                        std::cin>>rut;
+                        guardadodeficha.find(rut);
+
+                        break;
+                    case 2:
+
+                        break;
+                    default:
+                        break;
+                    }
+                }
 
                 break;
-            case 1: // quien debe pasar
-
-                if (urgencia.empty() == true)
-                {
-                    std::cout << normal.front() << "\n";
-                    
-                    std::cout << "1" << "\n";
-                    normal.dequeue();
-
-                    std::cout << "2" << "\n";
-                }
-                else
-                {
-
-                    std::cout << urgencia.front() << "\n";
-                    urgencia.dequeue();
-                }
-
-                break;
+            case 3:
+                return 0;
             default:
+
                 std::cout << "Error no siguio instrucciones" << "\n";
+
+                return 1;
                 break;
             }
-            break;
-        case 2:
-
-            break;
-        case 3:
-            return 0;
-        default:
-
-            std::cout << "Error no siguio instrucciones" << "\n";
-
-            return 1;
-            break;
         }
-    }
 
-} while (ingreso!=3);}
+    } while (ingreso != 3);
+}
