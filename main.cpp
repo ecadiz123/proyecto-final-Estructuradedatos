b2a1642b6fc282349e7d97f6d92e38eca1b453d4
ecadiz123 <ecadiz@ing.ucsc.cl>
2024-06-25 23:34:53 -0400

    se hicieron funciones las filas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "HashTable/HashEncadenado.hpp"
 #include "queue/ListQueue.hpp"
 #include <iostream>
+
 void printficha(element_t ficha) // Función para printear ficha
 {
     std::cout << "Rut:" << "\n"; // imprime rut
@@ -19,10 +20,62 @@ void printficha(element_t ficha) // Función para printear ficha
 
     std::cout << ficha.piezasatratar << "\n";
 }
+
+/*
+4 funciones de la app
+consultar fila de espera
+añadir a fila de espera
+
+consultar ficha
+añadir/eliminar/modificar ficha
+
+*/
+void consultafilas(ListQueue *urgencia, ListQueue *normal) // imprime al paciente que ahora le toca
+{
+    if ((*urgencia).empty() == true)
+    {
+        std::cout << (*normal).front() << "\n";
+
+        (*normal).dequeue();
+    }
+    else
+    {
+
+        std::cout << (*urgencia).front() << "\n";
+        (*urgencia).dequeue();
+    }
+}
+
+void ingresofilaespera(ListQueue *urgencia, ListQueue *normal) // clase que añade a alguien a la fila de espera
+{
+    std::string nombre;
+    int ingresofila, turno;
+
+    std::cout << "Ingrese nombre y apellido junto de paciente" << "\n";
+
+    std::cin >> nombre;
+    fflush(stdin); // limpia buffer
+
+    std::cout << "Ingrese a que fila lo desea enviar" << "\n";
+
+    std::cout << "1. Atencion Normal" << "\n";
+    std::cout << "2. Atencion de Urgencia" << "\n";
+
+    std::cin >> ingresofila;
+    fflush(stdin); // limpia buffer
+    if (ingresofila == 1)
+    {
+        (*normal).enqueue(nombre);
+    }
+    else
+        (*urgencia).enqueue(nombre);
+}
+
 int main()
 {
     HashEncadenado guardadodeficha; // tabla donde se van a guardar las fichas
     ListQueue normal, urgencia;     // fila de urgencia y la fila normal
+    ListQueue *pnormal=&normal, *purgencia=&urgencia;//punteros para cada fila para trabajar los valores por referencia en funciones
     int ingreso;
 
     // variables fila
@@ -32,7 +85,7 @@ int main()
     // variables fichas
     element_t fichaaux;
     int ingresoficha;
-    int rut;
+    key_t rut; // llave para buscar ficha
     int modificar;
     std::cout << "Bienvenido" << "\n";
     do // se repite hasta que salga
@@ -59,46 +112,12 @@ int main()
             {
                 switch (turno)
                 {
-                case 2: // Ingreso de pacientes
+                case 1: // quien debe pasar siguiente
 
-                    std::cout << "Ingrese nombre y apellido junto de paciente" << "\n";
-
-                    std::cin >> nombre;
-                    fflush(stdin); // limpia buffer
-
-                    std::cout << "Ingrese a que fila lo desea enviar" << "\n";
-
-                    std::cout << "1. Atencion Normal" << "\n";
-                    std::cout << "2. Atencion de Urgencia" << "\n";
-
-                    std::cin >> ingresofila;
-                    fflush(stdin); // limpia buffer
-                    if (ingresofila == 1)
-                    {
-                        normal.enqueue(nombre);
-                    }
-                    else
-                        urgencia.enqueue(nombre);
-
+                    consultafilas(&urgencia, &normal);
                     break;
-                case 1: // quien debe pasar
-
-                    if (urgencia.empty() == true)
-                    {
-                        std::cout << normal.front() << "\n";
-
-                        std::cout << "1" << "\n";
-                        normal.dequeue();
-
-                        std::cout << "2" << "\n";
-                    }
-                    else
-                    {
-
-                        std::cout << urgencia.front() << "\n";
-                        urgencia.dequeue();
-                    }
-
+                case 2: // Ingreso de pacientes
+                    ingresofilaespera(&urgencia, &normal);
                     break;
                 default:
                     std::cout << "Error no siguio instrucciones" << "\n";
@@ -110,7 +129,7 @@ int main()
                 std::cout << "Ingrese que desea hacer" << "\n";
 
                 std::cout << "1. Consultar ficha" << "\n";
-                std::cout << "2. Añadir/modificar/eliminar ficha" << "\n";
+                std::cout << "2. Ingresar/modificar/eliminar ficha" << "\n";
                 std::cin >> ingresoficha;
                 fflush(stdin); // limpia buffer
                 {
@@ -119,13 +138,15 @@ int main()
                     {
                     case 1: // consulta ficha
 
-                        std::cout << "Ingrese rut sin digito " << "\n";
+                        std::cout << "Ingrese rut sin digito verificador" << "\n";
                         std::cin >> rut;
                         fflush(stdin); // limpia buffer
                         fichaaux = guardadodeficha.find(rut);
                         if (fichaaux.rut != 0) // Si no encuentra la ficha devuelve una ficha sin rellenar de rut 0
-
                             printficha(fichaaux);
+                        else
+                            std::cout << "No se encontro ficha" << "\n";
+
                         break;
                     case 2: // ingreso/modifica/elimina ficha
 
@@ -140,8 +161,8 @@ int main()
                         {
                         case 1: // Ingresar ficha
 
-                            // se volvio requerimiento usar fflush para limpiar el buffer de entrada al trabajar con strings
-                            std::cout << "Ingrese Rut sin digito verificador" << "\n";
+                            // se volvio requerimiento usar fflush para limpiar el buffer de entrada al trabajar con tantas strings
+                            std::cout << "Ingrese Rut sin puntos ni digito verificador" << "\n";
                             std::cin >> fichaaux.rut;
                             fflush(stdin);
                             std::cout << "Ingrese digito verificador" << "\n";
